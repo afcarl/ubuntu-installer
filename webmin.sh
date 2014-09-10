f4a2d9062ed32e463d69e77dd7fd26599377c017
Frank Lemanschik <frank@github.dspeed.eu>
2014-09-10 02:45:43 +0200

    Create webmin.sh

---
+++ webmin.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+apt-get install wget curl apache2 php5 mysql-server
+echo Acquire::GzipIndexes "false"; > /etc/apt/apt.conf.d/docker-gzip-indexes
+ && apt-get update && apt-get install \
+ wget \
+ libnet-ssleay-perl \
+ openssl \
+ libauthen-pam-perl \
+ libio-pty-perl \
+ screen \
+ apt-show-versions \
+ python \
+ && wget http://prdownloads.sourceforge.net/webadmin/usermin_1.610_all.deb \
+ && wget wget http://prdownloads.sourceforge.net/webadmin/webmin_1.700_all.deb \
+ && dpkg --install webmin_1.700_all.deb \
+ && dpkg --install usermin_1.610_all.deb
