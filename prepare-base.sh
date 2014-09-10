0309ea0941cef9eeaed5b1ee97136da3d367d746
Frank Lemanschik <frank@github.dspeed.eu>
2014-09-10 03:01:50 +0200

    Update prepare-base.sh

--- prepare-base.sh
+++ prepare-base.sh
@@ -1,6 +1,6 @@
 export LC_ALL C
 export DEBIAN_FRONTEND noninteractive
-echo 'APT::Install-Recommends "0";
-APT::Get::Assume-Yes "true"; 
-APT::Get::force-yes "true"; 
+echo 'APT::Install-Recommends "0"; \n
+APT::Get::Assume-Yes "true"; \n
+APT::Get::force-yes "true"; \n
 APT::Install-Suggests "0";' > /etc/apt/apt.conf.d/01buildconfig
