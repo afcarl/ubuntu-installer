7aa8fa3319d2047fc12cb5780481c27834e98ffa
Frank Lemanschik <frank@github.dspeed.eu>
2014-09-10 03:10:17 +0200

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get install wget curl apache2 php5 mysql-server
-echo Acquire::GzipIndexes "false"; > /etc/apt/apt.conf.d/docker-gzip-indexes \
- && apt-get update && apt-get install \
+echo Acquire::GzipIndexes "false"; > /etc/apt/apt.conf.d/docker-gzip-indexes
+ apt-get update && apt-get install \
  wget \
  libnet-ssleay-perl \
  openssl \
@@ -9,8 +9,8 @@ echo Acquire::GzipIndexes "false"; > /etc/apt/apt.conf.d/docker-gzip-indexes \
  libio-pty-perl \
  screen \
  apt-show-versions \
- python \
- && wget http://prdownloads.sourceforge.net/webadmin/usermin_1.610_all.deb \
- && wget wget http://prdownloads.sourceforge.net/webadmin/webmin_1.700_all.deb \
- && dpkg --install webmin_1.700_all.deb \
- && dpkg --install usermin_1.610_all.deb
+ python 
+ wget http://prdownloads.sourceforge.net/webadmin/usermin_1.610_all.deb \
+ wget wget http://prdownloads.sourceforge.net/webadmin/webmin_1.700_all.deb \
+ dpkg --install webmin_1.700_all.deb \
+  dpkg --install usermin_1.610_all.deb
